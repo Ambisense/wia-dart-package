b3c83900b8bd49e2a2cdcb94b8fdcfc232c8612e
eimlav <eimlav123@gmail.com>
2020-07-08 10:33:34 +0100

    Add createdAt/updatedAt fields to Kiosk

--- lib/src/resources/kiosk.dart
+++ lib/src/resources/kiosk.dart
@@ -3,14 +3,28 @@ class Kiosk {
 
   String name;
 
+  DateTime createdAt;
+
+  DateTime updatedAt;
+
   Kiosk();
 
   Kiosk.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        name = json['name'];
+        name = json['name'],
+        createdAt = json['createdAt'] != null
+            ? DateTime.fromMillisecondsSinceEpoch(json['createdAt'],
+                isUtc: true)
+            : null,
+        updatedAt = json['updatedAt'] != null
+            ? DateTime.fromMillisecondsSinceEpoch(json['updatedAt'],
+                isUtc: true)
+            : null;
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
+        'createdAt': createdAt,
+        'updatedAt': updatedAt,
       };
 }
